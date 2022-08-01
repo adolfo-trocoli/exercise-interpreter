eb56f6d23801ffc8cee31ec670f0b07a7142f3a3
Adolfo Trocolí <adolfotrocoli@gmail.com>
2022-08-01 16:44:15 +0200

    Añadida opción de repeticiones totales

--- interprete_calistenia.py
+++ interprete_calistenia.py
@@ -9,6 +9,7 @@ def parse_arguments():
 	parser.add_argument('-m', '--month', help='month to show, format m or m/y')
 	parser.add_argument('-d', '--day', help='day to show, format d/m/y')
 	parser.add_argument('--pr', help='personal record of a certain exercise')
+	parser.add_argument('-t', '--total', help='total reps for a given exercise, requires -s', action='store_true')
 	return parser.parse_args()
 
 def is_header(line):
@@ -21,7 +22,7 @@ def print_dict():
 		print(f'{key[:-1]}')
 		for value in values:
 			print(f'\t{value}')
-	print()
+
 def add_value(line):
 	'''Adds a value to the dictionary.  '''
 	if header not in dict.keys():
@@ -37,6 +38,8 @@ def procesar_modo_busqueda(line):
 	if args.search in line:
 		contador_busqueda += 1
 		add_value(line.strip())
+		if args.total:
+			add_reps(line)
 
 def is_month(month, line):
 	return month == header_regex.search(line).group(2)
@@ -77,9 +80,25 @@ def procesar_modo_pr(line):
 	if is_pr(args.pr, line):
 		pr_string += header_regex.search(line).group(1) + ': ' + get_pr(line) + '\n'
 
+def add_reps(line):
+	global rep_count
+	result = rep_regex.search(line)
+	if result is None:
+		return
+	if result.group(2) is not None:
+		if result.group(5) is not None:
+			rep_count += int(result.group(2)) * int(result.group(3)) + int(result.group(5))
+		else:
+			rep_count += int(result.group(2)) * int(result.group(3))
+	elif result.group(5) is not None:
+		rep_count += int(result.group(1)) + int(result.group(5))
+	else:
+		rep_count += int(result.group(1))
 # main
 args = parse_arguments()
 header_regex = re.compile('(\d{1,2}/(\d{1,2}/\d{2})){1}(\s\(\w*\))?(\s-\s[\w\d\s:]*)?.*')
+rep_regex = re.compile('^[a-zA-Z\s]*((\d{1,2})x(\d{1,2})|\d{1,2})(\s?\+\s?(\d{1,2}))?')
+
 dict = dict()
 header = ''
 contador_busqueda = 0
@@ -87,6 +106,7 @@ contador_mes = 0
 day_string = ''
 selected_day = False
 pr_string = ''
+rep_count = 0
 
 modo_busqueda = args.search is not None
 modo_day = args.day is not None
@@ -105,19 +125,24 @@ with open(args.file) as file:
 		if(modo_pr):
 			procesar_modo_pr(line)
 
-	if(modo_busqueda):
+	if modo_busqueda:
 		print(f'\nMostrando ejercicio \'{args.search}\', se ha entrenado {contador_busqueda} veces en {len(dict)} entrenos:')
 		print('-----------------------------------------------------------------------------')
 		print_dict()
-	if(modo_mes):
+		if args.total:
+			print(f'El número total de repeticiones hechas de este ejercicio es {rep_count}\n')
+		else:
+			print()
+	if modo_mes:
 		print(f'Se entrenó {contador_mes} de los {dias_mes()} días del mes, un {int(contador_mes / dias_mes() * 100)}%\n')
-	if(modo_day):
+	if modo_day:
 		if len(day_string):
 			print(f'\nEl día {args.day} se hizo:\n{day_string}\n')
 		else:
 			print(f'\nEl día {args.day} no se entrenó\n')
-	if(modo_pr):
+	if modo_pr:
 		if len(pr_string):
 			print(f'\nSe conocen los siguientes PRs del ejercicio {args.pr}:\n' + pr_string)
 		else:
 			print(f'\nNo se tienen registros de PRs de \'{args.pr}\'')
+
