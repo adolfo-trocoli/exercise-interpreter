391e7440d081b0f0b46283d76b6e22bca26a39dd
Adolfo Trocolí <adolfotrocoli@gmail.com>
2022-08-01 13:57:14 +0200

    Añadida opción para mostrar PRs

--- interprete_calistenia.py
+++ interprete_calistenia.py
@@ -8,6 +8,7 @@ def parse_arguments():
 	parser.add_argument('-s', '--search', help='exercise to search for')
 	parser.add_argument('-m', '--month', help='month to show, format m or m/y')
 	parser.add_argument('-d', '--day', help='day to show, format d/m/y')
+	parser.add_argument('--pr', help='personal record of a certain exercise')
 	return parser.parse_args()
 
 def is_header(line):
@@ -64,6 +65,18 @@ def procesar_modo_day(line):
 	elif selected_day and line.strip():
 		day_string += ('\t') + line
 
+def is_pr(exercise, line):
+	return f'pr {exercise}' in line.lower() 
+
+def get_pr(line):
+	pr_regex = '^.*-[\s\w]*:\s*(\d*)'
+	return re.search(pr_regex, line).group(1)
+
+def procesar_modo_pr(line):
+	global pr_string
+	if is_pr(args.pr, line):
+		pr_string += header_regex.search(line).group(1) + ': ' + get_pr(line) + '\n'
+
 # main
 args = parse_arguments()
 header_regex = re.compile('(\d{1,2}/(\d{1,2}/\d{2})){1}(\s\(\w*\))?(\s-\s[\w\d\s:]*)?.*')
@@ -73,12 +86,15 @@ contador_busqueda = 0
 contador_mes = 0
 day_string = ''
 selected_day = False
+pr_string = ''
 
 modo_busqueda = args.search is not None
 modo_day = args.day is not None
 modo_mes =  args.month is not None
+modo_pr = args.pr is not None
 
 with open(args.file) as file:
+
 	for line in file:
 		if(modo_busqueda):
 			procesar_modo_busqueda(line)
@@ -86,6 +102,9 @@ with open(args.file) as file:
 			procesar_modo_mes(line)
 		if(modo_day):
 			procesar_modo_day(line)
+		if(modo_pr):
+			procesar_modo_pr(line)
+
 	if(modo_busqueda):
 		print(f'\nMostrando ejercicio \'{args.search}\', se ha entrenado {contador_busqueda} veces en {len(dict)} entrenos:')
 		print('-----------------------------------------------------------------------------')
@@ -94,6 +113,11 @@ with open(args.file) as file:
 		print(f'Se entrenó {contador_mes} de los {dias_mes()} días del mes, un {int(contador_mes / dias_mes() * 100)}%\n')
 	if(modo_day):
 		if len(day_string):
-			print(f'El día {args.day} se hizo:\n{day_string}\n')
+			print(f'\nEl día {args.day} se hizo:\n{day_string}\n')
 		else:
-			print(f'El día {args.day} no se entrenó\n')
+			print(f'\nEl día {args.day} no se entrenó\n')
+	if(modo_pr):
+		if len(pr_string):
+			print(f'\nSe conocen los siguientes PRs del ejercicio {args.pr}:\n' + pr_string)
+		else:
+			print(f'\nNo se tienen registros de PRs de \'{args.pr}\'')
